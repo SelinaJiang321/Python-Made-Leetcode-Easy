65e0d1a3a8811eb5092bf8f58523a68e07aac1b7
SelinaJiang321 <selinajiang321@gmail.com>
2021-02-04 15:23:31 +1100

    Update 226. Invert Binary Tree.py

--- easy/from 200 to 300/226. Invert Binary Tree.py
+++ easy/from 200 to 300/226. Invert Binary Tree.py
@@ -31,14 +31,22 @@ Google: 90% of our engineers use the software you wrote (Homebrew), but you canâ
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution(object):
     def invertTree1(self, root):
+        # create a new stack
+        # the head of the stack points to the root of the tree
+        # stack in python is built on a linked list structure
         stack = [root]
         while stack:
+            # pops out the "head" of the tree
             node = stack.pop()
+            # if node is not NULL
             if node:
+                # invert the left and the right of the "head"
                 node.left,node.right = node.right, node.left
-                stack.extend([node.right, node.left])\
+                # link the node.right and node.left to the "head" stack
+                stack.extend([node.right, node.left])
         
         return root
     def invertTree(self, root):
@@ -46,8 +54,8 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
-    
-    
+     
+        # invert the "leaf" of the root already
         if root:
             root.left, root.right = self.invertTree1(root.right), self.invertTree1(root.left)
         return root
