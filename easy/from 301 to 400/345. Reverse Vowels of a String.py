46f8b3ff89f27a24a28e74f665efede98cd4f985
SelinaJiang321 <selinajiang321@gmail.com>
2021-02-07 10:43:08 +1100

    Update 345. Reverse Vowels of a String.py

--- easy/from 301 to 400/345. Reverse Vowels of a String.py
+++ easy/from 301 to 400/345. Reverse Vowels of a String.py
@@ -20,17 +20,25 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        # transform the string to a list of vowels
         s = list(s)
+        # to create a set of vowels that can be used to compare
         vows = set('aeiouAEIOU')
         
         l, r = 0, len(s) - 1
         
+        
         while l <= r:
+            # loop until there are vowels
             while l <= r and s[l] not in vows: l += 1
             while l <= r and s[r] not in vows: r -= 1
             if l > r: break
+            # if there are vowels in place, just exchange their elements
             s[l], s[r] = s[r], s[l]
             l, r = l+1, r-1
-            
+        
+        # join the list as a string, same as "".join(s) here
+        # if you use ',' , then "hello" becomes "h,o,l,l,e"
         return ''.join(s)
             
+        # Any advice or comments?
