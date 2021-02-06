439a159782446b84cd5e1a52d842547a0cc383d4
SelinaJiang321 <selinajiang321@gmail.com>
2021-02-06 22:33:06 +1100

    Create 125. Valid Palindrome.py

---
+++ easy/from 101 to 200/125. Valid Palindrome.py
@@ -0,0 +1,34 @@
+"""
+
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+ 
+
+Constraints:
+
+s consists only of printable ASCII characters.
+"""
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        new_s = "".join([c for c in s if c.isalnum()])
+        new_s = new_s.lower()
+        return new_s == new_s[::-1]
+        
+        
