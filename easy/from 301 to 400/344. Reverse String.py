4817af149f8018dc9cb42d92ee049b5570b56288
SelinaJiang321 <selinajiang321@gmail.com>
2021-02-07 10:09:13 +1100

    Create 344. Reverse String.py

---
+++ easy/from 301 to 400/344. Reverse String.py
@@ -0,0 +1,49 @@
+"""
+
+
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+ 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+"""
+
+# Solution one:
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        
+        # life is short, use python lol
+        # constant space
+        return s.reverse()
+        
+# Solution two:
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        
+        # two pointer solution
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
+        
+    
