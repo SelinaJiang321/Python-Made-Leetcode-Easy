10e383f7201b3e5aebfde26075aedf9518241e45
SelinaJiang321 <selinajiang321@gmail.com>
2021-02-07 10:54:09 +1100

    Update 206. Reverse Linked List.py

--- easy/from 200 to 300/206. Reverse Linked List.py
+++ easy/from 200 to 300/206. Reverse Linked List.py
@@ -22,5 +22,17 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+        prev = None
+        node = head
         
+        while node:
+            curr = node.next
+            # Attention: don't write curr.next = node. then there will be a loop here and the curr can't move forward
+            # the logic here is move curr first, and then let node.next = prev
+            # then move prev and curr forward
+            # until node = None, return prev: the last valid node
+            node.next = prev
+            prev = node
+            node = curr
+       return prev
         
