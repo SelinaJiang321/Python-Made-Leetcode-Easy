fc536e6f2eb4bc9613e19843e3b1b9aaf58e5684
SelinaJiang321 <selinajiang321@gmail.com>
2021-02-07 23:18:56 +1100

    Create 217. Contains Duplicate.py

---
+++ easy/from 200 to 300/217. Contains Duplicate.py
@@ -0,0 +1,48 @@
+"""
+
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+"""
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # list.sort() and sorted(list) differences:
+        # list.sort(key=..., reverse=...)  sorted(list, key=..., reverse=...)
+        
+        """
+        Sort in Descending order
+        The sort() method accepts a reverse parameter as an optional argument.
+
+        Setting reverse = True sorts the list in the descending order.
+
+        list.sort(reverse=True)
+        Alternately for sorted(), you can use the following code.
+
+        sorted(list, reverse=True)
+        
+        """
+        n = sorted(nums)
+        for i in range(0,len(nums)-1, 1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
