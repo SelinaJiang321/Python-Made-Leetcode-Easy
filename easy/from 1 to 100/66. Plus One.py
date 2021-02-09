83d8738b4a00d97ff393aaae01fb124a75d88ce5
SelinaJiang321 <selinajiang321@gmail.com>
2021-02-09 15:36:34 +1100

    Create 66. Plus One.py

---
+++ easy/from 1 to 100/66. Plus One.py
@@ -0,0 +1,52 @@
+"""
+
+
+Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Example 3:
+
+Input: digits = [0]
+Output: [1]
+ 
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+
+"""
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        
+        for i in range(len(digits)-1, -1, -1) :
+            digits[i] += 1
+            if digits[i] < 10:
+                return digits
+            digits[i] += 0
+            
+        # similar to combine the strings 
+        return [1] + digits
+        
+        # Time complexity: O(1) 
+        # Space complexity: O(1)
