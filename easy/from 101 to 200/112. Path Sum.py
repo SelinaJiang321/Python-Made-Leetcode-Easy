3489120b34650ea897e86db53905b7c1c088ab3f
Selina Jiang <selinajiang321@gmail.com>
2021-02-10 14:21:34 +1100

    Create 112. Path Sum.py

---
+++ easy/from 101 to 200/112. Path Sum.py
@@ -0,0 +1,56 @@
+"""
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: bool
+        """
+        
+        # dfs
+        # if the tree is empty or we reach to the end but still can't find the sum
+        if not root: return False
+        if not root.left and not root.right and targetSum == root.val: return True
+        # checking whether it is a leaf and we got the targetSum
+        # recursion starts
+        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
+        
+        # Any comments or advice?
+        
