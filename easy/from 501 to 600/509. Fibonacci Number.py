ca48dd184fed320f25d7ef2ad7e50f52ba5e5a78
Selina Jiang <selinajiang321@gmail.com>
2021-02-13 22:13:43 +1100

    Create 509. Fibonacci Number.py

---
+++ easy/from 501 to 600/509. Fibonacci Number.py
@@ -0,0 +1,61 @@
+"""
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+
+Constraints:
+
+0 <= n <= 30
+
+"""
+
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        if n <= 1: return n
+        if n == 2: return 2
+        
+        current = 0
+        prev1 = 1
+        prev2 = 1
+        
+        for i range(3, n+1):
+            current = prev1 + prev2
+            prev1 = prev2
+            prev2 = current
+            
+        return current
+        
+        # just to switch the value
+      
+        # recursion is another way, but take longer time but less space
+        
+        
+
+
