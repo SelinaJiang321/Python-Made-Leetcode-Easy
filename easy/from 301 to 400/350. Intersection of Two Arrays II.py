654f8c54a69c9fa0e914f64be8f2765aaf7946b7
Selina Jiang <selinajiang321@gmail.com>
2021-02-24 22:56:12 +1100

    Create 350. Intersection of Two Arrays II.py

---
+++ easy/from 301 to 400/350. Intersection of Two Arrays II.py
@@ -0,0 +1,57 @@
+"""
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Note:
+
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+
+"""
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        
+        d1 = {}
+        d2 = {}
+        ansList = []
+        for i in nums1:
+            try:
+                x = d1[i]
+                d1[i] = x + 1
+            except KeyError:
+                d1[i] = 1
+        for i in nums2:
+            try:
+                x = d2[i]
+                d2[i] = x + 1
+            except KeyError:
+                d2[i] = 1
+        for i in d1:
+            if i in d2:
+                ansList.extend([i for x in range(min(d1[i],d2[i]))])
+            else:
+                pass
+        return ansList
+        
+        # Any comments?
+        
