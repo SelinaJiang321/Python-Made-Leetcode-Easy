62de32b98748f6b07bb495899753fce5ba1aae2f
Selina Jiang <selinajiang321@gmail.com>
2021-03-10 22:21:37 +1100

    Create 953. Verifying an Alien Dictionary.py

---
+++ high_frequency/953. Verifying an Alien Dictionary.py
@@ -0,0 +1,57 @@
+"""
+
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+
+ 
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+order.length == 26
+All characters in words[i] and order are English lowercase letters.
+
+
+"""
+
+class Solution(object):
+    def isAlienSorted(self, words, order):
+        """
+        :type words: List[str]
+        :type order: str
+        :rtype: bool
+        """
+        # mapping
+        # mapping of char to order: order = "hlabcdefgijkmnopqrstuvwxyz"
+        if not words or len(words) <2:
+            return True
+
+        vocab = {v: k for k, v in enumerate(order)}
+
+        word_index = [[vocab[char] for char in w] for w in words]
+
+        for w1, w2 in zip(word_index, word_index[1:]):
+            if w1 >= w2:
+                return False
+        return True
+      
+      #Time: O(C) Space:O(n)
