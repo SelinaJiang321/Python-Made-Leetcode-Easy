67fc7316bcc6630a46054c35f4adc5e8d0cbf990
Selina Jiang <selinajiang321@gmail.com>
2021-03-12 22:28:27 +1100

    Using range instead of xrange

--- high_frequency/811. Subdomain Visit Count.py
+++ high_frequency/811. Subdomain Visit Count.py
@@ -44,7 +44,7 @@ class Solution(object):
             count, domain = domain.split()
             count = int(count)
             frags = domain.split('.')
-            for i in xrange(len(frags)):
+            for i in range(len(frags)):
                 ans[".".join(frags[i:])] += count
 
         return ["{} {}".format(ct, dom) for dom, ct in ans.items()]
