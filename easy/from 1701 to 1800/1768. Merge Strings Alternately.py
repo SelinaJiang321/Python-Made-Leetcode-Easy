69122df8f6cf24f9af8c0e39a4496a705f11f90e
Selina Jiang <selinajiang321@gmail.com>
2021-03-30 22:59:55 +1100

    TAKE away

---
+++ easy/from 1701 to 1800/1768. Merge Strings Alternately.py
@@ -0,0 +1,57 @@
+"""
+
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
+"""
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        res = ""
+        for i in range(min(len(word1),len(word2))):
+            res  = res + word1[i] + word2[i]
+            
+        return res + word1[i+1:] + word2[i+1:]
+      
+      # take away: min function can be used and when return the result string, can just by using array slicing
+      
+      
+      
