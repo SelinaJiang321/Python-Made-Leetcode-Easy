8f24d184bb55627b76c8075a0e48f934285f5be7
Selina Jiang <selinajiang321@gmail.com>
2021-04-08 22:41:30 +1000

    Take away: list(enumerate()) function

---
+++ easy/from 401 to 500/496. Next Greater Element I.py
@@ -0,0 +1,58 @@
+"""
+
+You are given two integer arrays nums1 and nums2 both of unique elements, where nums1 is a subset of nums2.
+
+Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+
+The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, return -1 for this number.
+
+ 
+
+Example 1:
+
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation:
+For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+For number 1 in the first array, the next greater number for it in the second array is 3.
+For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+Example 2:
+
+Input: nums1 = [2,4], nums2 = [1,2,3,4]
+Output: [3,-1]
+Explanation:
+For number 2 in the first array, the next greater number for it in the second array is 3.
+For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+ 
+
+Constraints:
+
+1 <= nums1.length <= nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 104
+All integers in nums1 and nums2 are unique.
+All the integers of nums1 also appear in nums2.
+
+"""
+
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        
+        for i, n in list(enumerate(nums1)):
+            ind = nums2.index(n)
+            flag = False
+            for i in range(ind, len(nums2)):
+                if nums2[i] > n:
+                    result.append(nums2[i])
+                    flag = True
+                    break
+            if not flag:
+                result.append('-1')
+                
+        return result
+        
