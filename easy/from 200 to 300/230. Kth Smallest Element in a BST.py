60196da879ed8950081161dd1668b2cf175a01d4
Selina Jiang <selinajiang321@gmail.com>
2021-04-27 21:49:07 +1000

    #Takeaway:Iterative Inorder Traversal

---
+++ easy/from 200 to 300/230. Kth Smallest Element in a BST.py
@@ -0,0 +1,51 @@
+"""
+
+Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
+            
