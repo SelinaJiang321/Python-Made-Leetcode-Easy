6f09661b7fe8e47173e29868414623b56e6cc04b
Selina Jiang <selinajiang321@gmail.com>
2021-05-12 22:31:47 +1000

    #Takeaway:recursion with bfs

---
+++ easy/from 1 to 100/62. Unique Paths.py
@@ -0,0 +1,48 @@
+"""
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+ 
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+Example 3:
+
+Input: m = 7, n = 3
+Output: 28
+Example 4:
+
+Input: m = 3, n = 3
+Output: 6
+ 
+
+Constraints:
+
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 109.
+"""
+
+class Solution(object):
+    def __init__(self):
+        self.steps = {}
+    def uniquePaths(self, m, n):
+        if m == 1 or n == 1: return 1
+        if (m, n) in self.steps: return self.steps[(m, n)]
+        self.steps[(m, n)] = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        return self.steps[(m, n)]
+        
