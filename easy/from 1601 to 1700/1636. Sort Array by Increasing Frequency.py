dda08780dfa629aa72b4d9c9e5df269e2f64d9d0
Selina Jiang <selinajiang321@gmail.com>
2021-05-22 22:13:21 +1000

    #Lambda function can help us define how to sort the array

--- easy/from 1601 to 1700/1636. Sort Array by Increasing Frequency.py
+++ easy/from 1601 to 1700/1636. Sort Array by Increasing Frequency.py
@@ -54,3 +54,12 @@ class Solution(object):
         # Time: O(nlogn) Space: O(n)
         
         
+ class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        d = {}
+        for num in nums:
+            if num not in d:
+                d[num] = 1
+            else:
+                d[num] += 1
+        return sorted(sorted(nums, reverse=True), key=lambda x: d[x])
