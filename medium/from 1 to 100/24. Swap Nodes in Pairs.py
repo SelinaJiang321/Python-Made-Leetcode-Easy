e1dd242fe24c7e507e43aabe465360879919f1fa
Selina Jiang <selinajiang321@gmail.com>
2021-05-31 23:25:08 +1000

    #Takeaway: initialize a dummy node by ListNode(-1) and then dummy.next = head to ensure we return the correct head

---
+++ medium/from 1 to 100/24. Swap Nodes in Pairs.py
@@ -0,0 +1,57 @@
+"""
+
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+Example 2:
+
+Input: head = []
+Output: []
+Example 3:
+
+Input: head = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        dummy.next = head
+        prev = dummy
+        
+        while head and head.next:
+            first = head
+            second = head.next
+            #linked together
+            prev.next = second
+            first.next = second.next
+            second.next = first
+            
+            prev = first
+            head = first.next
+            
+        return dummy.next
+                
+        
