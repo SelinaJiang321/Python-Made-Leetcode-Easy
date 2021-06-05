9ec44d329b043f75d2e41f5286298f6a13d049c3
Selina Jiang <selinajiang321@gmail.com>
2021-06-05 11:12:07 +1000

    #backtracking :a DFS traversal in a n-ary tree
    1.classic backtracking method being used
           no previous nodes/reduce the time complexity
    2. when appending a list, it's better to convert the array/list into a list first by using res.append(list(combo))

---
+++ medium/from 1 to 100/39. Combination Sum.py
@@ -0,0 +1,73 @@
+"""
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+Example 4:
+
+Input: candidates = [1], target = 1
+Output: [[1]]
+Example 5:
+
+Input: candidates = [1], target = 2
+Output: [[1,1]]
+ 
+
+Constraints:
+
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+All elements of candidates are distinct.
+1 <= target <= 500
+
+"""
+
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
+        res = []
+        
+        # remain of this formula, combination of the list, start of the index
+        def backtrack(remain, comb, start):
+            if remain == 0:
+                print(comb)
+                res.append(list(comb))
+                print(res)
+            elif remain < 0:
+                return
+            
+            for i in range(start, len(candidates)):
+                comb.append(candidates[i])
+                backtrack(remain - candidates[i],comb,i)
+                comb.pop()
+                
+                
+        backtrack(target, [],0)
+        
+        return res
