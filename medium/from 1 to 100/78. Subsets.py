9458afa6ee3097822a69dae0166c59130ca80b74
Selina Jiang <selinajiang321@gmail.com>
2021-06-13 22:21:40 +1000

    #cascading/backtracking: similar questions with the combinations of the digits

---
+++ medium/from 1 to 100/78. Subsets.py
@@ -0,0 +1,39 @@
+"""
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+
+"""
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = [[]]
+        N = len(nums)
+        
+        for num in nums:
+            res += [curr + [num] for curr in res]
+            
+        return res
+        
